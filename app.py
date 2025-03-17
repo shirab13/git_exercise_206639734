02e52cd183f625f8c64bd23c9ba1b4ca94186f7f
shirab13 <shirab13@ac.sce.ac.il>
2025-03-17 10:40:19 +0200

    additional print function

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ def add_numbers(a, b):
 if __name__ == "__main__":
     print(f"3 + 5 = {add_numbers(3, 5)}")
  
+
 def multiply_numbers(a, b): 
     return a * b 
+
+print("This is a simple addition function") 
+
